cfbe1dc6c1677aaeba73d4ad5303dcb07f3234e7
Maanasa Bhamidi <maanasabhamidi@gmail.com>
2026-06-28 21:49:29 -0400

    initial CDK VPC setup

--- cdk_vpc_project/cdk_vpc_project_stack.py
+++ cdk_vpc_project/cdk_vpc_project_stack.py
@@ -1,19 +1,26 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+from aws_cdk import(
+   Stack,
+   aws_ec2 as ec2,
 )
 from constructs import Construct
-
 class CdkVpcProjectStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+   def __init__(self, scope: Construct,construct_id: str, **kwargs)-> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkVpcProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(
+            self, "MyVpc",
+            max_azs=2,
+            nat_gateways=0,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Public",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24
+                ),
+                ec2.SubnetConfiguration(
+                    name="Private",
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidr_mask=24
+                )
+            ]
+        )    
