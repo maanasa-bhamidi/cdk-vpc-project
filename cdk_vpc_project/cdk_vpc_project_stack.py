3e14511d8836acdb0b1de12bd54b0d62ff15b58b
Maanasa Bhamidi <maanasabhamidi@gmail.com>
2026-06-30 17:41:09 -0400

    CDk VPC project with security group and IAM role

--- cdk_vpc_project/cdk_vpc_project_stack.py
+++ cdk_vpc_project/cdk_vpc_project_stack.py
@@ -1,10 +1,13 @@
-from aws_cdk import(
-   Stack,
-   aws_ec2 as ec2,
+from aws_cdk import (
+    Stack,
+    aws_ec2 as ec2,
+    aws_iam as iam,
 )
 from constructs import Construct
+
 class CdkVpcProjectStack(Stack):
-   def __init__(self, scope: Construct,construct_id: str, **kwargs)-> None:
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         vpc = ec2.Vpc(
@@ -23,4 +26,32 @@ class CdkVpcProjectStack(Stack):
                     cidr_mask=24
                 )
             ]
-        )    
+        )
+
+        security_group = ec2.SecurityGroup(
+            self, "MySecurityGroup",
+            vpc=vpc,
+            description="Allow SSH and HTTP traffic",
+            allow_all_outbound=True
+        )
+
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(80),
+            description="Allow HTTP traffic from anywhere"
+        )
+
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(22),
+            description="Allow SSH access"
+        )
+
+        role = iam.Role(
+            self, "MyRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com")
+        )
+
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess")
+        )
